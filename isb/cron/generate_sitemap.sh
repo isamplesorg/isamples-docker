69669c88be6de2a9d89fdb49031797939edeeb54
Danny Mandel <danny.mandel@gmail.com>
2024-07-12 15:18:50 -0700

    Additional changes for sitemaps

--- isb/cron/generate_sitemap.sh
+++ isb/cron/generate_sitemap.sh
@@ -2,6 +2,7 @@
 export PYTHONPATH=/app
 cd /app
 mkdir /var/log/isamples
+export ORCID_TOKEN_SECRET=`cat /run/secrets/orcid_token_secret`
 declare DATE=$(date "+%Y-%m-%d")
 declare OUTPUT_DIR="/app/sitemaps/$DATE/sitemaps"
 echo "Attempting to remove sitemap directory output at $OUTPUT_DIR, ok if this fails"
@@ -9,6 +10,7 @@ rm -rf $OUTPUT_DIR
 echo "Creating sitemap directory output at $OUTPUT_DIR"
 mkdir -p $OUTPUT_DIR
 echo "Invoking sitemap creation script, using sitemap prefix of $ISB_SITEMAP_PREFIX"
+curl "http://localhost:8000/export/create_sitemap" -H "Authorization: Bearer $ORCID_TOKEN_SECRET"
 /usr/local/bin/python /app/scripts/generate_things_sitemap.py -h "$ISB_SITEMAP_PREFIX" -p "$OUTPUT_DIR"  >& /var/log/isamples/$DATE.sitemap.txt
 echo "Removing old sitemaps symlink"
 rm -rf /app/isb_web/sitemaps
