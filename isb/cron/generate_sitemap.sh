77d1a25dd9a4e8de9e90d7ba44571e83ca07a836
Danny Mandel <danny.mandel@gmail.com>
2024-07-16 13:46:25 -0700

    Assorted fixes after getting things running on henry (#70)

--- isb/cron/generate_sitemap.sh
+++ isb/cron/generate_sitemap.sh
@@ -11,7 +11,6 @@ echo "Creating sitemap directory output at $OUTPUT_DIR"
 mkdir -p $OUTPUT_DIR
 echo "Invoking sitemap creation script, using sitemap prefix of $ISB_SITEMAP_PREFIX"
 curl "http://localhost:8000/export/create_sitemap" -H "Authorization: Bearer $ORCID_TOKEN_SECRET"
-/usr/local/bin/python /app/scripts/generate_things_sitemap.py -h "$ISB_SITEMAP_PREFIX" -p "$OUTPUT_DIR"  >& /var/log/isamples/$DATE.sitemap.txt
 echo "Removing old sitemaps symlink"
 rm -rf /app/isb_web/sitemaps
 echo "Creating new sitemaps symlink"
